597a698036b4b8fd2d6b020eca50b3689ab46abe
gopi <gopireddyb08@gmail.com>
2024-10-26 14:37:32 +0530

    injection

---
+++ src/main/java/com/nt/Bootproj01DependencyInjectionApplication.java
@@ -0,0 +1,13 @@
+package com.nt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bootproj01DependencyInjectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bootproj01DependencyInjectionApplication.class, args);
+	}
+
+}
